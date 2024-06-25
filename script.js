cc5f6bf79e45da8ba364558bf5976e8a7e148741
mbachibueze <mbachibueze27@gmail.com>
2024-06-25 16:51:45 +0100

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// toggle Menu
+var navLinks = document.getElementById("navLinks")
+
+const showMenu = () =>{
+  navLinks.style.right="0"
+  navLinks.style.display="block"
+}
+
+const hideMenu = () =>{
+  navLinks.style.right="-200px"
+  navLinks.style.display="none"
+}
